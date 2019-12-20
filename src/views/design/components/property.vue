26ed421a2dc3476d675883f15522642f55c473fb
cloudyan <1395093509@qq.com>
2019-12-20 14:00:21 +0800

    feat: add tabs

--- src/views/design/components/property.vue
+++ src/views/design/components/property.vue
@@ -1,9 +1,6 @@
 <template>
   <el-container class="design-property">
-    <el-tabs v-model="activeName" @tab-click="handleClick" class="p-tabs" stretch>
-      <el-tab-pane class="p-item" label="属性" name="first">属性</el-tab-pane>
-      <el-tab-pane class="p-item" label="脚本" name="second">脚本</el-tab-pane>
-    </el-tabs>
+    属性编辑
   </el-container>
 </template>
 
--- src/views/design/index.vue
+++ src/views/design/index.vue
@@ -3,16 +3,42 @@
     <el-header class="design-header flex-middle">设计器</el-header>
     <el-main class="design-main">
       <el-container class="design-container h100">
-        <el-aside class="design-left">
-          <d-widget
-            :widget="widget"
-          />
+        <el-aside class="design-left" style="width: 250px;">
+          <el-tabs
+            v-model="tabAside"
+            @tab-click="handleClick"
+            class="p-tabs"
+            stretch
+            fixed
+          >
+            <el-tab-pane class="p-item" label="组件" name="widget">
+              <d-widget
+                :widget="widget"
+              />
+            </el-tab-pane>
+            <el-tab-pane class="p-item" label="页面" name="page">
+              页面
+            </el-tab-pane>
+          </el-tabs>
         </el-aside>
         <el-main class="design-center">main</el-main>
-        <el-aside class="design-right">
-          <d-property
-            :schema="schema"
-          />
+        <el-aside class="design-right" style="width: 300px;">
+          <el-tabs
+            v-model="tabProperty"
+            @tab-click="handleClick"
+            class="p-tabs"
+            stretch
+            fixed
+          >
+            <el-tab-pane class="p-item" label="属性" name="property">
+              <d-property
+                :schema="schema"
+              />
+            </el-tab-pane>
+            <el-tab-pane class="p-item" label="脚本" name="script">
+              脚本
+            </el-tab-pane>
+          </el-tabs>
         </el-aside>
       </el-container>
     </el-main>
@@ -20,12 +46,6 @@
 </template>
 
 <script>
-// 设计器
-// 各区域:
-//   header
-//   侧边栏 -> 组件列表 页面管理
-//   主编辑区 -> 导入JSON 生成JSON 生成代码 预览 重置 保存 + 区
-//   属性编辑区 -> 属性 脚本
 import schema from './schema'
 import DWidget from './components/widget'
 import DProperty from './components/property'
@@ -45,12 +65,23 @@ export default {
     return {
       widget: schema.propsSchema,
       schema: {},
+      tabAside: 'widget',
+      tabProperty: 'script',
     }
+  },
+
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event)
+    },
   },
 }
 </script>
 
 <style lang="stylus" scoped>
+
+// $designAsideWidth = 250px;
+
 .design-header {
   background-color: #1278f6;
   color: #fff;
@@ -61,17 +92,30 @@ export default {
   height: calc(100% - 60px);
 }
 
-.design-left {
-  margin: 0;
-  width: 250px;
-  padding: 8px 0 8px 10px;
-}
-
+.design-left,
 .design-right {
   margin: 0;
   padding: 0;
-  width: 300px;
-  border-left: 1px solid #ccc;
+  overflow-y: scroll;
   background: none;
+}
+
+.design-left {
+  box-shadow: 1px 0 0 #ccc;
+}
+
+.design-right {
+  box-shadow: -1px 0 0 #ccc;
+}
+
+.p-tabs[fixed] {
+  background: #fff;
+  >>> .el-tabs__nav-wrap {
+    width: 250px;
+  }
+}
+
+.p-item {
+  padding: 0 8px;
 }
 </style>
