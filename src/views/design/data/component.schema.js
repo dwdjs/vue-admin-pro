f51839bbe9d417cbe34c493508020ceb94b042f2
cloudyan <1395093509@qq.com>
2019-12-30 10:01:01 +0800

    feat: add components.schema

---
+++ src/views/design/data/component.schema.js
@@ -0,0 +1,56 @@
+// 组件库, 如 form-render by element-ui
+export const widgets = {
+
+}
+
+// 映射关系
+export const mapping = {
+
+}
+
+// 配置项: 组件 区块 模板
+export const options = {
+  // 通用字段
+  input: {},
+  textarea: {},
+  number: {},
+  inputNumber: {},
+  uploadImages: {},
+  uploadFile: {},
+  radio: {},
+  checkbox: {},
+  imageRadio: {},
+  imageCheckbox: {},
+  time: {},
+  date: {},
+  dateTime: {},
+  switch: {},
+  select: {},
+  cascader: {},
+  tree: {},
+  color: {},
+  richtext: {},
+  editor: {},
+  rate: {},
+  slider: {},
+  // 联系人字段
+  username: {},
+  password: {},
+  mobile: {},
+  email: {},
+  phone: {},
+  address: {},
+  link: {},
+  desc: {},
+  // 布局组件
+  layout: {},
+  container: {},
+  tabs: {},
+  divider: {},
+  pageBreak: {},
+}
+
+// 配置数据
+export const schema = {
+
+}
